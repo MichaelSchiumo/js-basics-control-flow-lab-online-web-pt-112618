464ee26209967321426e4f14c4a22508aba5578f
Michael Schiumo <mschiumo18@gmail.com>
2019-06-10 14:15:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  const ride = 199;
+  let price;
+
+    if (ride < 400) {
+    price = 'This one is on me!';
+    }
+  }
+
 
 function ternaryCheckCity(){
   // Write your code here!
