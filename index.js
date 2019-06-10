476529220a0f80cdf8eb79133c548cf4e9ea7818
Michael Schiumo <mschiumo18@gmail.com>
2019-06-10 15:07:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-  const ride = 199;
-  let price;
-
     if (ride < 400) {
-    price = 'This one is on me!';
+      return 'This one is on me!'
+    }
+    else if (ride > 2000 && ride < 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+    else {
+      return "No can do."
     }
   }
 
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(charm){
   // Write your code here!
+  switch (charm) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return 'Bye.'
+  }
 }
